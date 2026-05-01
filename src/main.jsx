e28aff14894eb99a4540c7dd0b55c70803a9dc6c
gun <gunnain147@gmail.com>
2026-05-01 15:44:07 +0700

    enable PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './pages/App.jsx'
 import './style.css'
+
+import { registerSW } from 'virtual:pwa-register'
+registerSW()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
